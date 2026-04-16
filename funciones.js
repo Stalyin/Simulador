d395eed16c6de12223088e65ecbdd37c0927a208
Stalyin <stalyin16ac@gmail.com>
2026-04-16 22:33:33 +0600

    feat:Show available funds and payment capacity in simulator

--- funciones.js
+++ funciones.js
@@ -1,1 +1,15 @@
-//AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos) {
+  let valorDisponible = 0;
+  valorDisponible = ingresos - egresos;
+
+  if (valorDisponible < 0) {
+    return 0;
+  }
+
+  return valorDisponible;
+}
+
+function calcularCapacidadPago(montoDisponible) {
+  let capacidadPago = montoDisponible * 0.5;
+  return capacidadPago;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,17 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+  let ingresos = obtenerValores("txtIngresos");
+  let egresos = obtenerValores("txtEgresos");
+  let disponible = calcularDisponible(ingresos, egresos).toFixed(2);
+  let capacidadPago = calcularCapacidadPago(disponible).toFixed(2);
+
+  mostrarEnPantalla("spnDisponible", disponible);
+  mostrarEnPantalla("spnCapacidadPago", capacidadPago);
+}
+
+function obtenerValores(id) {
+  return parseFloat(document.getElementById(id).value);
+}
+
+function mostrarEnPantalla(id, valor) {
+  document.getElementById(id).innerHTML = valor;
+}
